a422ec8e9e418f59bbd80bca0446f49154d12c6e
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2024-06-21 01:39:36 +0200

    app use hono logger

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import { logger } from "hono/logger";
 
 export const app = new Hono();
 
+app.use("*", logger());
 app.get("/hello", (c) => {
   return c.json({ message: "Hello, World!" });
 });
