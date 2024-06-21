a855467454a64775e5cd55b9170e88cb815f35fa
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2024-06-21 02:12:22 +0200

    zod + hono zod validator on expense post request

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,11 +1,23 @@
+import type { Expense } from "@/types/expense";
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
+import { z } from "zod";
+
+const expensePostSchema = z.object({
+  title: z.string().min(3),
+  value: z.number().positive(),
+  description: z.string().optional(),
+  date: z.coerce.date(),
+  category: z.string(),
+});
 
 export const expenses = new Hono()
   .get("/", (c) => {
     return c.json({ exprenses: [{ value: 4.8 }] });
   })
-  .post("/", (c) => {
-    return c.json({});
+  .post("/", zValidator("json", expensePostSchema), async (c) => {
+    const expense = await c.req.valid("json");
+    return c.json(expense);
   })
   .delete("/", (c) => {
     return c.json({});
---
+++ types/expense.d.ts
@@ -0,0 +1,7 @@
+export type Expense = {
+  title: string;
+  value: number;
+  description?: string;
+  date: Date;
+  category: string;
+};
