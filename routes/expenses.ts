f6d3e9edf85b153dbab29315fccd80e1e57804f9
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2024-06-23 00:09:24 +0200

    dynamic route param with regex + mocked data

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -3,7 +3,35 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
 
+const fakeExpenses: Expense[] = [
+  {
+    id: 1,
+    title: "Coffee",
+    value: 4.8,
+    description: "Coffee at Starbucks",
+    date: new Date(),
+    category: "Food & Drinks",
+  },
+  {
+    id: 2,
+    title: "Lunch",
+    value: 12.5,
+    description: "Lunch at McDonalds",
+    date: new Date(),
+    category: "Food & Drinks",
+  },
+  {
+    id: 3,
+    title: "Dinner",
+    value: 20,
+    description: "Dinner at Olive Garden",
+    date: new Date(),
+    category: "Food & Drinks",
+  },
+];
+
 const expensePostSchema = z.object({
+  id: z.number().int().positive(),
   title: z.string().min(3),
   value: z.number().positive(),
   description: z.string().optional(),
@@ -21,4 +49,8 @@ export const expenses = new Hono()
   })
   .delete("/", (c) => {
     return c.json({});
+  })
+  .get("/:id{[0-9]+}", (c) => {
+    const id = c.req.param("id");
+    return c.json(fakeExpenses.find((e) => e.id === Number(id)));
   });
--- types/expense.d.ts
+++ types/expense.d.ts
@@ -1,4 +1,5 @@
 export type Expense = {
+  id: number;
   title: string;
   value: number;
   description?: string;
