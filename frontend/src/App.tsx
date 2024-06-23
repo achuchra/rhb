82124e0ebc22cb1cbb039889a375fefdb88fb222
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2024-06-23 20:52:23 +0200

    App buttons + tailwind config adjustment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,27 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="flex flex-col items-center gap-2">
+      <h1 className="text-2xl font-bold">Vite + React</h1>
+      <div className="flex flex-col gap-2">
+        <button
+          className="bg-stone-500 hover:bg-stone-600 px-8 py-2 rounded-md text-white"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Increase
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button
+          className="bg-stone-500 hover:bg-stone-600 px-8 py-2 rounded-md text-white"
+          onClick={() => setCount((count) => count - 1)}
+        >
+          Decrease
+        </button>
+        <p className="text-center">Count is {count}</p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-center">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
