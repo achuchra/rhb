f07af3b4809f33262286ab8d090926795e49ea0c
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2024-06-24 00:25:19 +0200

    hono serve static file so 3000 tests prod build and 5173 is still dev

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,13 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenses } from "./routes/expenses";
+import { serveStatic } from "hono/bun";
 
 export const app = new Hono();
 
 app.use("*", logger());
-app.get("/hello", (c) => {
-  return c.json({ message: "Hello, World!" });
-});
+
 app.route("/api/v1/expenses", expenses);
+
+app.get("*", serveStatic({ root: "./frontend/dist" }));
+app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
