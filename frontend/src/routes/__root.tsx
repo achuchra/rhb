b42dff952589cc2708bc8ef12b32b104c9773b61
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2024-06-30 17:26:37 +0200

    tanstack router init config + extra routes

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,19 +2,46 @@ import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: Root,
+});
+
+function Navbar() {
+  return (
+    <div className="flex gap-2 justify-center bg-primary-foreground p-2">
+      <Link
+        to="/"
+        className="[&.active]:underline [&.active]:underline-offset-4 hover:text-primary/50 [&.active]:hover:text-primary"
+      >
+        Home
+      </Link>
+      <Link
+        to="/expenses"
+        className="[&.active]:underline [&.active]:underline-offset-4 hover:text-primary/50 [&.active]:hover:text-primary"
+      >
+        Expenses
+      </Link>
+      <Link
+        to="/create-expense"
+        className="[&.active]:underline [&.active]:underline-offset-4 hover:text-primary/50 [&.active]:hover:text-primary"
+      >
+        Create
+      </Link>
+      <Link
+        to="/about"
+        className="[&.active]:underline [&.active]:underline-offset-4 hover:text-primary/50 [&.active]:hover:text-primary"
+      >
+        About
+      </Link>
+    </div>
+  );
+}
+
+function Root() {
+  return (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
+      <Navbar></Navbar>
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,66 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { api } from "@/lib/api";
+import { useQuery } from "@tanstack/react-query";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const callTotalSpent = async () => {
+  const res = await api.expenses["total-spent"].$get();
+  if (!res.ok) {
+    throw new Error("Failed to fetch total spent");
+  }
+  return await res.json();
+};
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const [count, setCount] = useState(0);
+
+  const { data, error, isPending, isRefetching } = useQuery({
+    queryKey: ["total-spent"],
+    queryFn: callTotalSpent,
+  });
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
+    <Card className="w-[30rem] mx-auto">
+      <CardHeader>
+        <CardTitle>Vite + React</CardTitle>
+        <CardDescription>Test app</CardDescription>
+      </CardHeader>
+      <CardContent className="flex justify-center gap-4">
+        <Button onClick={() => setCount((count) => count + 1)}>Increase</Button>
+        <Button
+          variant={"outline"}
+          onClick={() => setCount((count) => count - 1)}
+        >
+          Decrease
+        </Button>
+      </CardContent>
+      <CardFooter className="flex flex-col items-center justify-center">
+        <p>Result: {count} </p>
+        <div className="mt-4">
+          {isPending || isRefetching ? (
+            <Skeleton className="h-6 w-[50px]" />
+          ) : (
+            `$${data.total}`
+          )}
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
